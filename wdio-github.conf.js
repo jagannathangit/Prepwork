963fedc0c02fcdf3043bf6613cdeffb2bda07052
jaganatwork <jayatwork1978@gmail.com>
2022-08-04 07:26:15 +0530

    changes for element not interactable in headless mode in github actions
    changes for element not interactable in headless mode in github actions

--- wdio-github.conf.js
+++ wdio-github.conf.js
@@ -10,7 +10,7 @@ exports.config = {
       acceptInsecureCerts: true,
       // We need to extends some Chrome flags in order to tell Chrome to run headless
       'goog:chromeOptions': {
-        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage',"--window-size=1920,1080","--start-maximized"],
       },
     },
   ],
